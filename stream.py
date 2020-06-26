dafbd612f41465a054f44e8bbf6038f0cfc0c3fb
sean <seanpollock3344@gmail.com>
2020-06-26 10:18:48 -0500

    simple stream

--- stream.py
+++ stream.py
@@ -1,30 +1,13 @@
 import time
 import cv2
 
-# "nvarguscamerasrc auto-exposure=1 wbmode=1 saturation=1.5 tnr-mode=2 tnr-strength=2 tnr-mode=     2 ee-mode=2 ee-strength=0.5 !"
-
-def gstreamer_pipeline(
-    capture_width=1920,
-    capture_height=1080,
-    flip_method=2,
-):
-    return (
-        "nvarguscamerasrc wbmode=2 !"
-        # "video/x-raw(memory:NVMM), "
-        # "width=(int)%d, height=(int)%d, "
-        # "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        # "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            flip_method,
-        )
-    )
-
-
 def stream_camera():
-    pipeline = gstreamer_pipeline()
+    pipeline = (
+        "nvarguscamerasrc wbmode=2 !"
+        "nvvidconv flip-method=2 ! "
+        "videoconvert ! video/x-raw, format=(string)BGR !"
+        "appsink"
+    )
     video_capture = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
     print('video_capture', video_capture)
     if video_capture.isOpened():
@@ -48,5 +31,4 @@ def stream_camera():
         print("could not open camera")
 
 if __name__ == "__main__":
-    # initiate gstreamer pipeline to CSI camera
     stream_camera()
